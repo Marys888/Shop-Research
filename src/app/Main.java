a907e842fddc785d4b298e058584f3cca0aa2028
Marys888 <severovamaha1999@gmail.com>
2025-05-05 13:36:58 +0200

    Add data handler implementation to process both sales and products data

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,17 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
+        DataProvider dataProvider = new DataProvider();
+        DataHandler dataHandler = new DataHandler();
 
+
+
+        String namesOutput = dataHandler.handleData(dataProvider.getProductNames());
+        getOutput("Products: " + namesOutput);
+        String salesOutput = dataHandler.handleData(dataProvider.getSalesAmounts());
+        getOutput("Sales, EUR: " + salesOutput);
+    }
+    private static void getOutput(String output) {
+        System.out.println(output);
     }
 }
